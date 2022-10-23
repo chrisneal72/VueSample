70fab59b32fef543d3adfb372906a3cc2cbed5c3
chrisneal72 <voodoodaddy2000@gmail.com>
2022-10-23 10:18:55 -0700

    Removed extra giitignore

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:5000/" : '',
+  // baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:5000/" : '',
+  baseURL: '',
   headers: {
     "Content-type": "application/json"
   }
