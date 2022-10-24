1b379ccbb09e84a8269ef544119ba01966ca0805
chrisneal72 <voodoodaddy2000@gmail.com>
2022-10-23 19:14:51 -0700

    config stuff

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:5000/" : '',
-  baseURL: '',
+  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:5000/" : '',
+  // baseURL: '',
   headers: {
     "Content-type": "application/json"
   }
